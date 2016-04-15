9994096c21a443aceacc723f9b8da8f85de4ce7a
Alexander Nørskov Larsen <alexnlarsen@gmail.com>
2016-04-15 10:16:37 +0200

    Initial work on implementing PageRank

---
+++ PageRank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamSharp.steamStore.models;
+using System.IO;
+
+namespace PageRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TagHandler tagHandler = new TagHandler(200);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PageRank/TagHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PageRank
+{
+    class TagHandler
+    {
+        public List<int> ListOfGameIDs; 
+        public TagHandler(int numberOfGames)
+        {
+            StreamReader reader = new StreamReader("appIDFileCSV.csv");
+            string[] ListOfIDs = reader.ReadLine().Split(',').Take(numberOfGames).ToArray();
+            ListOfGameIDs = new List<int>(ListOfIDs.Select(int.Parse).ToList());
+            reader.Close();
+        }
+    }
+}
